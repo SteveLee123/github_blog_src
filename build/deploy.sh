e740fb48bb89a8ef1db13356609ad23843a230e2
lcxc <1597374034@qq.com>
2026-01-10 22:47:33 +0800

    first commit

---
+++ build/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git config user.name 'lcxc'
+git config user.email '1597374034@qq.com'
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:SteveLee123/github_blog_src.git master:gh-pages
+
+cd -
